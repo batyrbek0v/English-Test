94ddf8e36fce505a7b9a5015fbaf602ab14654d0
Erlan <erlankambarov94@gmail.com>
2022-01-12 21:09:14 +0300

    End the English Test

---
+++ JS/script.js
@@ -0,0 +1,430 @@
+var a = 0
+var ask = alert("Приветствую друг мой. Пройди тест на знание английского. Переведите слова 'Существительные' ")
+// Вопрос 1
+var test = prompt('Time - ?')
+if(test === 'Время'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Время"')
+}
+
+// Вопрос 2
+var test = prompt('Person - ?')
+if(test === 'Человек'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Человек"')
+}
+
+// Вопрос 3
+var test = prompt('Letter - ?')
+if(test === 'Письмо'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Письмо"')
+}
+
+// Вопрос 4
+var test = prompt('Way - ?')
+if(test === 'Путь' || test === 'Дорога'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Дорога"')
+}
+
+// Вопрос 5
+var test = prompt('Men - ?')
+if(test === 'Мужчины'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Мужчины"')
+}
+
+// Вопрос 6
+var test = prompt('Thing - ?')
+if(test === 'Вещь'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Вещь"')
+}
+
+// Вопрос 7
+var test = prompt('Man - ?')
+if(test === 'Мужчина'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Мужчина"')
+}
+
+// Вопрос 8
+var test = prompt('North - ?')
+if(test === 'Север'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Север"')
+}
+
+// Вопрос 9
+var test = prompt('Life - ?')
+if(test === 'Жизнь'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Жизнь"')
+}
+
+// Вопрос 10
+var test = prompt('Order - ?')
+if(test === 'Заказ'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Заказ"')
+}
+
+// Вопрос 11
+var test = prompt('Part - ?')
+if(test === 'Часть'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Часть"')
+}
+
+// Вопрос 12
+var test = prompt('Child - ?')
+if(test === 'Ребенок'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Ребенок"')
+}
+
+// Вопрос 13
+var test = prompt('Fact - ?')
+if(test === 'Факт'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Факт"')
+}
+
+// Вопрос 14
+var test = prompt('Eye - ?')
+if(test === 'Глаза'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Глаза"')
+}
+
+// Вопрос 15
+var test = prompt('Woman - ?')
+if(test === 'Женщина'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Женщина"')
+}
+
+// Вопрос 16
+var test = prompt('Place - ?')
+if(test === 'Место'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Место"')
+}
+
+// Вопрос 17
+var test = prompt('Week - ?')
+if(test === 'Неделя'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Неделя"')
+}
+
+// Вопрос 18
+var test = prompt('Case - ?')
+if(test === 'Дело' || test === 'Случай'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Дело / Случай"')
+}
+
+// Вопрос 19
+var test = prompt('Point - ?')
+if(test === 'Точка' || test === 'Смысл'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Точка / Смысл"')
+}
+
+// Вопрос 20
+var test = prompt('Government - ?')
+if(test === 'Государство'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Государство"')
+}
+
+// Вопрос 21
+var test = prompt('Company - ?')
+if(test === 'Компания'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Компания"')
+}
+
+// Вопрос 22
+var test = prompt('Number - ?')
+if(test === 'Номер'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Номер"')
+}
+
+// Вопрос 23
+var test = prompt('Group - ?')
+if(test === 'Группа'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Группа"')
+}
+
+// Вопрос 24
+var test = prompt('Problem - ?')
+if(test === 'Проблема'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Проблема"')
+}
+
+// Вопрос 25
+var test = prompt('Law - ?')
+if(test === 'Закон'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Закон"')
+}
+
+// Вопрос 26
+var test = prompt('Community - ?')
+if(test === 'Община' || test === 'Сообщество'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Община / Община"')
+}
+
+// Вопрос 27
+var test = prompt('Area - ?')
+if(test === 'Область' || ' Территория'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Область / Территория"')
+}
+
+// Вопрос 28
+var test = prompt('Study - ?')
+if(test === ' Учеба'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Учеба"')
+}
+// Вопрос 29
+var test = prompt('Research - ?')
+if(test === 'Исследование'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Исследование"')
+}
+// Вопрос 30
+var test = prompt('Air - ?')
+if(test === 'Воздух'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Воздух"')
+}
+// Вопрос 31
+var test = prompt('Force - ?')
+if(test === 'Сила'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Сила"')
+}
+// Вопрос 32
+var test = prompt('Education - ?')
+if(test === 'Образование'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Образование"')
+}
+// Вопрос 33
+var test = prompt('Guy - ?')
+if(test === 'Парень'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Парень')
+}
+// Вопрос 34
+var test = prompt('Kind - ?')
+if(test === 'Сорт' || 'Класс'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Сорт / Класс"')
+}
+// Вопрос 35
+var test = prompt('Country - ?')
+if(test === 'Страна'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Страна"')
+}
+// Вопрос 36
+var test = prompt('Piece - ?')
+if(test === 'Кусок'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Кусок"')
+}
+// Вопрос 37
+var test = prompt('Side - ?')
+if(test === 'Сторона'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Сторона"')
+}
+// Вопрос 38
+var test = prompt('Line - ?')
+if(test === 'Линия'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Линия"')
+}
+// Вопрос 39
+var test = prompt('Reason - ?')
+if(test === 'Причина'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Причина"')
+}
+// Вопрос 40
+var test = prompt('Service - ?')
+if(test === 'Услуга'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Услуга"')
+}
+// Вопрос 41
+var test = prompt('Others - ?')
+if(test === 'Другие'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Другие"')
+}
+// Вопрос 42
+var test = prompt('Art - ?')
+if(test === 'Искусство'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Искусство"')
+}
+// Вопрос 43
+var test = prompt('East - ?')
+if(test === 'Восток'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Восток"')
+}
+// Вопрос 44
+var test = prompt('Example - ?')
+if(test === 'Пример'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Пример"')
+}
+// Вопрос 45
+var test = prompt('Feet - ?')
+if(test === 'Ноги'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Ноги"')
+}
+// Вопрос 46
+var test = prompt('Science - ?')
+if(test === 'Наука'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Наука"')
+}
+// Вопрос 47
+var test = prompt('Pie - ?')
+if(test === 'Пирог'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Пирог"')
+}
+// Вопрос 48
+var test = prompt('Brick - ?')
+if(test === 'Кирпич'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Кирпич"')
+}
+// Вопрос 49
+var test = prompt('Gate - ?')
+if(test === 'Ворота'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно! ' + ' ✔ "Ворота"')
+}
+// Вопрос 50
+var test = prompt('badge - ?')
+if(test === 'Значок'){
+    a++
+    alert('Правильно!')
+}else{
+    alert('Неправильно ' + ' Правильно "Значок"')
+}
+var res = alert("Вы ответили правильно на  " + a + " вопросов " + " из 50")
